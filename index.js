15f1490f79aa20e677aa92b9d14798bf5fcbe5a2
asafecoubelle <acoubelle@gmail.com>
2023-10-18 23:13:00 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let nomeDoHeroi = "Homem Aranha";
+let xpDoHeroi = 4000;
+let nivel;
+
+if (xpDoHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    nivel = ("Imortal");
+} else {
+    nivel = ("Radiante");
+}
+
+console.log("O Herói de nome" + " " + nomeDoHeroi + " " + "está no nível de" + " " + nivel)
